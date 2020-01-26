b910d4fd144971a2d1437643c4af58b8cecb4bfa
Gil Brudner <57257745+indoorrobot@users.noreply.github.com>
2020-01-26 22:51:56 +0200

    Update kvsWebRTCClientMasterGstreamerSample.c

--- samples/kvsWebRTCClientMasterGstreamerSample.c
+++ samples/kvsWebRTCClientMasterGstreamerSample.c
@@ -131,8 +131,8 @@ PVOID sendGstreamerAudioVideo(PVOID args)
     switch (pSampleConfiguration->mediaType) {
         case SAMPLE_STREAMING_VIDEO_ONLY:
             pipeline = gst_parse_launch(
-                    "autovideosrc num-buffers=1000 ! queue ! videoconvert ! video/x-raw,width=1280,height=720,framerate=30/1 ! x264enc bframes=0 speed-preset=veryfast key-int-max=30 bitrate=512 ! "
-                    "video/x-h264,stream-format=byte-stream,alignment=au,profile=baseline ! appsink sync=TRUE emit-signals=TRUE name=appsink-video",
+                    "v4l2src device=/dev/video3 ! queue ! h264parse ! appsink sync=TRUE emit-signals=TRUE name=appsink-video",
+                    //"video/x-h264,stream-format=byte-stream,alignment=au,profile=baseline ! appsink sync=TRUE emit-signals=TRUE name=appsink-video",
                     &error);
             break;
 
